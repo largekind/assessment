327b99af778aa1f5cbca1ff849785a485d8e6bf1
lark <42988298+largekind@users.noreply.github.com>
2022-09-19 21:58:14 +0900

    Update Assessment

--- assessment.js
+++ assessment.js
@@ -49,15 +49,31 @@ assessmentButton.onclick = () => {
   console.log('ボタンが押された')
   // result-areaを一度消去
   resultDivided.innerText = ''
-  // tweet-area領域に診断結果表示エリア作成
-  const header = document.createElement('h3');
-  header.innerText = '診断結果'
-  resultDivided.appendChild(header);
+
+  /* result-area領域に診断結果表示エリア作成
+  bootstrapにあるCardの例に従って整形していく */
+  //// headerDivided の作成
+  const headerDivided = document.createElement('div');
+  headerDivided.setAttribute('class', 'card-header');
+  headerDivided.innerText = '診断結果';
+
+  //// bodyDivided の作成
+  const bodyDivided = document.createElement('div');
+  bodyDivided.setAttribute('class', 'card-body');
 
   const paragraph = document.createElement('p');
+  paragraph.setAttribute('class', 'card-text');
   const result = assessment(userName);
   paragraph.innerText = result;
-  resultDivided.appendChild(paragraph)
+  bodyDivided.appendChild(paragraph);
+
+  //// resultDivided に Bootstrap のスタイルを適用する
+  resultDivided.setAttribute('class', 'card');
+  resultDivided.setAttribute('style', 'max-width: 700px;')
+
+  //// headerDivided と bodyDivided を resultDivided に差し込む
+  resultDivided.appendChild(headerDivided);
+  resultDivided.appendChild(bodyDivided);
 
   //ツイートエリア作成
   tweetDivided.innerText = '';
